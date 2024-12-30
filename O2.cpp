8df84c9e3a398aafc00cd5f026ce64fff63dc24e
Murat <muratguler864@gmail.com>
2024-12-30 01:17:47 +0100

    Commit 2

--- O2.cpp
+++ O2.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+class xmas_object {
+    public:
+        void do_something() {
+
+        }
+};
+
+class xmas_tree : public xmas_object {
+
+};
+
+class santaclaus : public xmas_object {
+
+};
+
+class present : public xmas_object {
+
+};
+
+class child : public xmas_object {
+
+};
+
 int main() {
     
 }
